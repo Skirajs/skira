88a4486174fb59a0823f3620ccbf6010a702fad7
Mitchell de Rijcke <mitchell@de.rijcke.org>
2016-03-02 09:06:56 +0100

    Some fixes

--- core/loader.js
+++ core/loader.js
@@ -2,13 +2,29 @@ if (typeof _babelPolyfill == "undefined") {
 	require("babel-polyfill");
 }
 
-var o = require(process.env.ENGINE);
+const applySite = require("site");
+const EventEmitter = require("eventemitter3-collector");
+const jade = require("jade/runtime");
+const merge = require("merge");
 
-if (typeof o == "function") {
-	var site = require("site");
-	o = o(site);
+function genSite() {
+	var site = applySite(jade, new EventEmitter());
+
+	
+
+	return site;
 }
 
-if (process.env.EXPORT) {
-	module.exports = o;
-}
+module.exports = (function() {
+	var engine = require(process.env.ENGINE);
+
+	if (engine) {
+		return genSite();
+	}
+
+	if (typeof engine == "function") {
+		return engine(genSite());
+	}
+
+	return engine;
+})();
